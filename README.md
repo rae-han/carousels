c80e0b7e560a8ec5690a41921dc18f5687de8e1a
raehan <raehan900809@gmail.com>
2022-11-27 17:56:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# environment config
+npm init
+npm i next
+
+```markdown
+# scripts in package.json
+"build": "npx next build",
+"dev": "npx next dev -p 3000"
+"start": "npx next start -p 3000"
+```
+
+npm run build
+npm run dev
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Components
+import Carousel from "../components/Carousel";
+
+function Index () {
+  return (
+    <div>
+      <header>header</header>
+      <Carousel></Carousel>
+    </div>
+
+  )
+}
+
+export default Index;
